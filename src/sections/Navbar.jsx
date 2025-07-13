a74a6861369d60f729cb5d94e266f0e73e51c114
sandeep350 <sandeepbiswal350@gmail.com>
2025-07-13 20:16:40 +0530

    first commit

---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed inset-x-0 z-20 w-full backdrop-blur-lg bg-primary/40'>Navbar
+    <div className="mx-auto c-space max-w-7xl">
+      <di className="flex items-center justify-between py-2"></di>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
